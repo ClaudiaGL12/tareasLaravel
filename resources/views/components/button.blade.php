2539a91a8c77381b616e48a4874da238f3603396
ClaudiaGL12 <claudia.gomlu@gmail.com>
2025-01-28 14:43:46 +0100

    segunda tarea 2v componente button

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,15 +1,34 @@
-@switch($anyadirIncome['type'])
-    @case('input')
-        <!-- Input button con redirección -->
-        <input type="button" value="Añadir Income" onclick="window.location.href='{{ $anyadirIncome['enlace'] }}'" class="btn btn-primary"/>
-        @break
+{{-- primera forma
+<div>
+    @switch($anyadirIncome['type'])
+        @case('input')
+            <input type="button" value="Añadir Income" onclick="window.location.href='{{ $anyadirIncome['enlace'] }}'" class="btn btn-primary"/>
+            @break
 
-    @case('button')
-        <!-- Button con redirección -->
-        <button type="button" onclick="window.location.href='{{ $anyadirIncome['enlace'] }}'" class="btn btn-primary">Añadir Income</button>
-        @break
+        @case('button')
+            <button type="button" onclick="window.location.href='{{ $anyadirIncome['enlace'] }}'" class="btn btn-primary">Añadir Income</button>
+            @break
 
-    @default
-        <!-- A link -->
-        <a href="{{ $anyadirIncome['enlace'] }}" class="bg-white">Añadir Income</a>
-@endswitch
+        @default
+            <a href="{{ $anyadirIncome['enlace'] }}" class="bg-white">Añadir Income</a>
+    @endswitch
+</div> --}}
+
+
+<div>
+    @if($attributes->has('href'))
+        <a {{$attributes->has('class')?$attributes:$attributes->merge(['class'=>'bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'])}}>
+            {{ $slot }}
+        </a>    
+    @elseif($attributes->has('name'))
+        <button type="submit" {{ $attributes->has('class')? $attributes: $attributes->merge(['class' => 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded']) }}>
+            {{ $slot }}
+        </button>
+    @else
+        <button type="button" {{ $attributes->has('class')? $attributes: $attributes->merge(['class' => 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded']) }}>
+            {{ $slot }}
+        </button>
+    @endif
+</div>
+
+          
--- resources/views/income/index.blade.php
+++ resources/views/income/index.blade.php
@@ -1,5 +1,21 @@
 {{--Si el constructor de estos no te pide ningúna variable, podemos añadirlos igual, pero para llamarlos dentro de ellos sería $attributes->get('nomVariable')--}}
 <x-layouts.index :title="$title">
   <x-table :tableData="$tableData"/>
-  <x-button :anyadirIncome="$anyadirIncome"/>
+
+  {{-- primera forma 
+    <x-button :anyadirIncome="$anyadirIncome">
+    Añadir income
+    </x-button> botón anterior 
+  --}}
+
+  <div style="mt-4"/>
+  <x-button href="https://laravel.com">
+    Add income
+  </x-button>
+  <div style="m-4">
+  <x-button href="https://laravel.com" class="clase1 clase2">
+    Add income
+  </x-button>
+
+
 </x-layouts.index>
